7310ee6db00d9ce001825912cba7935b38c85ab1
arash.sw90@gmail.com <arash.sw90@gmail.com>
2022-10-28 20:13:51 +0330

    init project

---
+++ src/test/java/com/blubank/doctorappointment/domain/entity/DoctorEntityFactoryTest.java
@@ -0,0 +1,78 @@
+package com.blubank.doctorappointment.domain.entity;
+
+import com.blubank.doctorappointment.domain.exception.EmptyFullNameException;
+import com.blubank.doctorappointment.domain.exception.InvalidStartAndEndTimeException;
+import com.blubank.doctorappointment.domain.exception.NullMedicalNoException;
+import com.blubank.doctorappointment.domain.vo.*;
+import org.hibernate.engine.jdbc.dialect.spi.DatabaseMetaDataDialectResolutionInfoAdapter;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import java.time.LocalTime;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+/**
+ * Created by arash on 28.10.22.
+ */
+
+public class DoctorEntityFactoryTest extends EntityFactoryTest{
+
+
+
+    @Test
+    @DisplayName("givenSimpleId_WhenCreateDoctorEntity_ThenDoctorEntityIdIsEqualsWithSimpleId")
+    public void givenSimpleId_WhenCreateDoctorEntity_ThenDoctorEntityIdIsEqualsWithSimpleId() {
+        Doctor doctorWithSimpleId = Doctor.of(id);
+        assertEquals(doctorWithSimpleId.getId(), ID.of(id));
+    }
+
+    @Test
+    @DisplayName("givenMedicalNoAndFullName_WhenCreateDoctorEntity_ThenDoctorEntityIsEqualsWithMedicalNoAndFullName")
+    public void givenMedicalNoAndFullName_WhenCreateDoctorEntity_ThenDoctorEntityIsEqualsWithMedicalNoAndFullName() {
+        Doctor doctorWithMedicalNoAndFullName = Doctor.of(id, medicalNo, fullName);
+        assertEquals(doctorWithMedicalNoAndFullName.getMedicalNo().getMedicalNo(), MedicalNo.of(medicalNo).getMedicalNo());
+        assertEquals(doctorWithMedicalNoAndFullName.getFullName().getFullName(), FullName.of(fullName).getFullName());
+    }
+    @Test
+    @DisplayName("givenNullMedicalNo_WhenCreateDoctorEntity_ThenDoctorEntityWillThrowsNullMedicalNoException")
+    public void givenNullMedicalNo_WhenCreateDoctorEntity_ThenDoctorEntityWillThrowsNullMedicalNoException(){
+        assertThrowsExactly(NullMedicalNoException.class,() -> Doctor.of(id,null,fullName));
+    }
+
+    @Test
+    @DisplayName("givenNullFullName_WhenCreateDoctorEntity_ThenDoctorEntityWillThrowsEmptyFullNameException")
+    public void givenNullFullName_WhenCreateDoctorEntity_ThenDoctorEntityWillThrowsEmptyFullNameException() {
+        assertThrowsExactly(EmptyFullNameException.class, () -> Doctor.of(id, medicalNo, null));
+    }
+
+    @Test
+    @DisplayName("givenInValidOpenTimeWithDurationLessThan30Min_WhenCreateDoctorEntityAndAddOpenTime_ThenOpenTimeIsEmpty")
+    public void givenInValidOpenTimeWithDurationLessThan30Min_WhenCreateDoctorEntityAndAddOpenTime_ThenOpenTimeIsEmpty() {
+        Doctor doctor = Doctor.of(id, medicalNo, fullName);
+        doctor.addOpenTimes(invalidDuration);
+        assertNull(doctor.getAppointments());
+    }
+    @Test
+    @DisplayName("givenInValidOpenTimeWithStartTimeLessThanEndTime_WhenCreateDoctorEntityAndAddOpenTime_ThenThrowsInvalidStartAndEndTimeException")
+    public void givenInValidOpenTimeWithStartTimeLessThanEndTime_WhenCreateDoctorEntityAndAddOpenTime_ThenThrowsInvalidStartAndEndTimeException() {
+        assertThrowsExactly(InvalidStartAndEndTimeException.class, () -> {
+            Doctor doctor = Doctor.of(id, medicalNo, fullName);
+            doctor.addOpenTimes(invalidStartingPointTime);
+        });
+    }
+    @Test
+    @DisplayName("givenValidOpenThanEndTime_WhenCreateDoctorEntityAndAddOpenTime_ThenDurationIsChunkedInto30MinWhichExpected")
+    public void givenValidOpenThanEndTime_WhenCreateDoctorEntityAndAddOpenTime_ThenDurationIsChunkedInto30MinWhichExpected() {
+        Doctor doctor = Doctor.of(id, medicalNo, fullName, new ArrayList<>());
+        doctor.addOpenTimes(validOpenTime);
+        assertNotNull(doctor.getAppointments());
+        assertEquals(doctor.getAppointments().size(), NumberOfExpectation);
+    }
+
+
+}
---
+++ src/test/java/com/blubank/doctorappointment/domain/entity/EntityFactoryTest.java
@@ -0,0 +1,65 @@
+package com.blubank.doctorappointment.domain.entity;
+
+import com.blubank.doctorappointment.domain.vo.Appointment;
+import com.blubank.doctorappointment.domain.vo.OpenTime;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.TestInstance;
+
+import java.time.LocalDate;
+import java.time.LocalTime;
+import java.util.List;
+
+
+/**
+ * Created by arash on 28.10.22.
+ */
+@TestInstance(TestInstance.Lifecycle.PER_CLASS)
+public abstract class EntityFactoryTest {
+
+    protected static Long id;
+    protected static Long medicalNo;
+    protected static String fullName;
+    protected static OpenTime validOpenTime;
+    protected static OpenTime invalidDuration;
+    protected static OpenTime invalidStartingPointTime;
+    protected static int NumberOfExpectation = 4;
+    protected static List<Appointment> expectedAppointments;
+
+    @BeforeAll
+    public static void init() {
+        id = 100L;
+        medicalNo = 123456789L;
+        fullName = "arash ariani";
+        validOpenTime = createValidOpenTime();
+        invalidDuration = createInValidDurationOpenTime();
+        invalidStartingPointTime = createInvalidStaringPointTime();
+        expectedAppointments = getExpectedAppointment();
+
+    }
+
+    private static OpenTime createInvalidStaringPointTime() {
+        return OpenTime.of(LocalDate.of(2022, 1, 1), LocalTime.of(10, 0), LocalTime.of(9, 30));
+    }
+
+    public static OpenTime createValidOpenTime() {
+        return OpenTime.of(LocalDate.of(2022, 1, 1), LocalTime.of(9, 0), LocalTime.of(11, 0));
+    }
+    public static OpenTime createInValidDurationOpenTime() {
+        return OpenTime.of(LocalDate.of(2022, 1, 1), LocalTime.of(9, 0), LocalTime.of(9, 15));
+    }
+    public static List<Appointment> getExpectedAppointment() {
+        return List.of(Appointment.of(Doctor.of(id,medicalNo,fullName),null,
+                OpenTime.of(validOpenTime.getVisitDate().getVisitDate(),
+                        LocalTime.of(9,0),LocalTime.of(9,30)),0),
+                Appointment.of(Doctor.of(id,medicalNo,fullName),null,
+                        OpenTime.of(validOpenTime.getVisitDate().getVisitDate(),
+                                LocalTime.of(9,30),LocalTime.of(10,0)),0),
+                Appointment.of(Doctor.of(id,medicalNo,fullName),null,
+                        OpenTime.of(validOpenTime.getVisitDate().getVisitDate(),
+                                LocalTime.of(10,0),LocalTime.of(10,30)),0),
+                Appointment.of(Doctor.of(id,medicalNo,fullName),null,
+                        OpenTime.of(validOpenTime.getVisitDate().getVisitDate(),
+                                LocalTime.of(10,30),LocalTime.of(11,0)),0));
+    }
+
+}
